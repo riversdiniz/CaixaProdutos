50eee49fa38febecab2274e65a1a40f7bce7d19e
River Diniz <river-s-diniz@hotmail.com>
2022-08-20 15:19:59 -0300

    Trabalhancom com arrays

--- src/com/caixa/view/Caixa.java
+++ src/com/caixa/view/Caixa.java
@@ -3,11 +3,13 @@ package com.caixa.view;
 public class Caixa {
     public static void main(String[] args) {
 
-        int qtdProdutos = 5;
-        int registro = 0;
+        float valores[] = new float[5];
+        valores[0] = 10;
+        valores[1] = 20;
+        valores[2] = 30;
+        valores[3] = 40;
+        valores[4] = 50;
 
-        for (int i = 0; i < qtdProdutos; i++) {
-            System.out.println("O caixa refistrou o produto" + i);
-        }
+        System.out.println(valores[4]);
     }
 }
